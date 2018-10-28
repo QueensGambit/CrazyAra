4a1d0e92b56dd5c21e435c2533ffe8e499742c1a
noelben <25zyutsb@gmail.com>
2018-10-29 03:02:34 +0800

    Improve time control handling Added winc and binc in movetime calculation Use moves left to calculate movetime Remove option max_search_time_s Modify playouts_empty_pockets default value to 8192 Modify playouts_filled_pockets default value to 8192 Modify playouts_update_stats default value to 1 Modify threshold_time_for_raw_net_ms default value to 50 Added option move_overhead_ms Added option moves_left Increase movetime by lowering the moves left when prev move score is bad Supports uci movetime command Use info string when sending messages to console per uci specs Fix or condition on quit and exit

--- crazyara.py
+++ crazyara.py
@@ -15,6 +15,15 @@ import chess.pgn
 
 import traceback
 
+
+# Constants
+MIN_SEARCH_TIME_MS = 100
+INC_FACTOR = 7
+INC_DIV = 8
+MOVES_LEFT = 40
+MIN_MOVES_LEFT = 10
+MAX_BAD_POS_VALUE = -0.10
+
 client = {
     'name': 'CrazyAra',
     'version': '0.2.0',
@@ -77,10 +86,8 @@ print(INTRO_PT2, end="")
 mcts_agent = None
 rawnet_agent = None
 gamestate = None
-
-switched_to_raw_net = False
-
 setup_done = False
+bestmove_value = None
 
 # SETTINGS
 s = {
@@ -90,18 +97,19 @@ s = {
     "context": 'cpu',
     "use_raw_network": False,
     "threads": 8,
-    "playouts_empty_pockets": 256,
-    "playouts_filled_pockets": 512,
-    "playouts_update_stats": 256,
+    "playouts_empty_pockets": 8192,
+    "playouts_filled_pockets": 8192,
+    "playouts_update_stats": 1,
     "centi_cpuct": 100,
     "centi_dirichlet_epsilon": 10,
     "centi_dirichlet_alpha": 20,
-    "max_search_time_s": 300,
     "max_search_depth": 40,
     "centi_temperature": 0,
     "centi_clip_quantil": 0,
     "virtual_loss": 3,
-    "threshold_time_for_raw_net_ms": 30000
+    "threshold_time_for_raw_net_ms": 50,
+    "move_overhead_ms": 50,
+    "moves_left": MOVES_LEFT
 }
 
 
@@ -129,10 +137,10 @@ def setup_network():
 
         mcts_agent = MCTSAgent(net, cpuct=s['centi_cpuct'] / 100, playouts_empty_pockets=s['playouts_empty_pockets'],
                                playouts_filled_pockets=s['playouts_filled_pockets'], max_search_depth=s['max_search_depth'],
-                               playouts_update=s['playouts_update_stats'], max_search_time_s=s['max_search_time_s'],
-                               dirichlet_alpha=s['centi_dirichlet_alpha'] / 100, dirichlet_epsilon=s['centi_dirichlet_epsilon'] / 100,
-                               virtual_loss=s['virtual_loss'], threads=s['threads'], temperature=s['centi_temperature'] / 100,
-                               clip_quantil=s['centi_clip_quantil'] / 100)
+                               playouts_update=s['playouts_update_stats'], dirichlet_alpha=s['centi_dirichlet_alpha'] / 100,
+                               dirichlet_epsilon=s['centi_dirichlet_epsilon'] / 100, virtual_loss=s['virtual_loss'],
+                               threads=s['threads'], temperature=s['centi_temperature'] / 100,
+                               clip_quantil=s['centi_clip_quantil'] / 100, min_movetime=MIN_SEARCH_TIME_MS)
 
         gamestate = GameState()
 
@@ -145,11 +153,13 @@ def perform_action(cmd_list):
     :return: 
     """
 
-    global switched_to_raw_net
     global AGENT
     global gamestate
     global mcts_agent
     global rawnet_agent
+    global bestmove_value
+
+    movetime_ms = MIN_SEARCH_TIME_MS
 
     if len(cmd_list) >= 5:
         if cmd_list[1] == 'wtime' and cmd_list[3] == 'btime':
@@ -157,25 +167,45 @@ def perform_action(cmd_list):
             wtime = int(cmd_list[2])
             btime = int(cmd_list[4])
 
-            if gamestate.is_white_to_move() is True:
+            my_inc, winc, binc = 0, 0, 0
+            if 'winc' in cmd_list:
+                winc = int(cmd_list[6])
+            if 'binc' in cmd_list:
+                binc = int(cmd_list[8])
+
+            if gamestate.is_white_to_move():
                 my_time = wtime
+                my_inc = winc
             else:
                 my_time = btime
+                my_inc = binc
 
-            if switched_to_raw_net is False and int(my_time) < s['threshold_time_for_raw_net_ms']:
-                log_print('Switching to raw network for fast mode...')
-                # switch to RawNetwork-Agent
-                switched_to_raw_net = True
+            moves_left = s['moves_left']
 
-            elif switched_to_raw_net is True and my_time >= s['threshold_time_for_raw_net_ms']:
-                log_print('Switching back to MCTS network for slow mode...')
-                # switch to RawNetwork-Agent
-                switched_to_raw_net = False
+            # Increase movetime by reducing the moves left if our prev bestmove value is below 0.0
+            if bestmove_value is not None and bestmove_value <= MAX_BAD_POS_VALUE:
+                moves_left -= abs(bestmove_value) * MOVES_LEFT
+                moves_left = max(moves_left, MIN_MOVES_LEFT)
+                print('info string Reduce moves left to %d' % moves_left)
 
-    if switched_to_raw_net is True or s['use_raw_network'] is True:
+            movetime_ms = max(my_time/moves_left + INC_FACTOR*my_inc//INC_DIV - s['move_overhead_ms'], MIN_SEARCH_TIME_MS)
+
+    # movetime in UCI protocol, go movetime x, search exactly x mseconds
+    # UCI protocol: http://wbec-ridderkerk.nl/html/UCIProtocol.html
+    elif len(cmd_list) == 3 and cmd_list[1] == 'movetime':
+        movetime_ms = max(int(cmd_list[2]) - s['move_overhead_ms'], MIN_SEARCH_TIME_MS)
+
+    mcts_agent.update_movetime(movetime_ms)
+    log_print('info string Time for this move is %dms' % movetime_ms)
+
+    if s['use_raw_network'] or movetime_ms < s['threshold_time_for_raw_net_ms']:
+        log_print('info string Using raw network for fast mode...')
         value, selected_move, confidence, _ = rawnet_agent.perform_action(gamestate)
     else:
         value, selected_move, confidence, _ = mcts_agent.perform_action(gamestate)
+
+    # Save the bestmove value [-1.0 to 1.0] to modify the next movetime
+    bestmove_value = float(value)
 
     log_print('bestmove %s' % selected_move.uci())
 
@@ -250,19 +280,20 @@ while True:
                 log_print('option name UCI_Variant type combo default crazyhouse var crazyhouse')
                 log_print('option name context type combo default cpu var cpu var gpu')
                 log_print('option name use_raw_network type check default false')
-                log_print('option name threads type spin default 8 min 1 max 4096')
-                log_print('option name playouts_empty_pockets type spin default 256 min 56 max 8192')
-                log_print('option name playouts_filled_pockets type spin default 512 min 56 max 8192')
-                log_print('option name playouts_update_stats type spin default 256 min 56 max 8192')
+                log_print('option name threads type spin default %d min 1 max 4096' % s['threads'])
+                log_print('option name playouts_empty_pockets type spin default %d min 56 max 8192' % s['playouts_empty_pockets'])
+                log_print('option name playouts_filled_pockets type spin default %d min 56 max 8192' % s['playouts_filled_pockets'])
+                log_print('option name playouts_update_stats type spin default %d min 1 max 8192' % s['playouts_update_stats'])
                 log_print('option name centi_cpuct type spin default 100 min 1 max 500')
                 log_print('option name centi_dirichlet_epsilon type spin default 10 min 0 max 100')
                 log_print('option name centi_dirichlet_alpha type spin default 20 min 0 max 100')
-                log_print('option name max_search_time_s type spin default 300 min 1 max 9000')
                 log_print('option name max_search_depth type spin default 40 min 1 max 100')
                 log_print('option name centi_temperature type spin default 0 min 0 max 100')
                 log_print('option name centi_clip_quantil type spin default 0 min 0 max 100')
                 log_print('option name virtual_loss type spin default 3 min 0 max 10')
-                log_print('option name threshold_time_for_raw_net_ms type spin default 30000 min 1 max 999999999')
+                log_print('option name threshold_time_for_raw_net_ms type spin default %d min 1 max 999999999' % s['threshold_time_for_raw_net_ms'])
+                log_print('option name move_overhead_ms type spin default %d min 0 max 60000' % s['move_overhead_ms'])
+                log_print('option name moves_left type spin default %d min 10 max 320' % s['moves_left'])
 
                 # verify that all options have been sent
                 log_print('uciok')
@@ -271,7 +302,7 @@ while True:
                 setup_network()
                 log_print('readyok')
             elif main_cmd == 'ucinewgame':
-                pass
+                bestmove_value = None
             elif main_cmd == "position":
                 setup_gamestate(cmd_list)
             elif main_cmd == "setoption":
@@ -284,7 +315,7 @@ while True:
                     traceback_text = traceback.format_exc()
                     log_print(traceback_text)
                     sys.exit(-1)
-            elif main_cmd == 'quit' or 'exit':
+            elif main_cmd == 'quit' or main_cmd == 'exit':
                 sys.exit(0)
             else:
                 # give the user a message that the command was ignored
