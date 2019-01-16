486abe5cfb97c15492630fc890abbc80564a2a3f
Matuiss2 <mtsbrbr@gmail.com>
2019-01-16 10:02:35 -0200

    Update module call, comment outdated ones, refactor repeated stuff

--- DeepCrazyhouse/src/tests/move_plane_round_trip_tests.py
+++ DeepCrazyhouse/src/tests/move_plane_round_trip_tests.py
@@ -1,9 +1,9 @@
 import unittest
 import chess
 from chess.variant import CrazyhouseBoard
-from src.domain.util import get_board_position_index, get_row_col
-from DeepCrazyhouse.src.model.crazy_house.output_representation import get_move_planes
-from DeepCrazyhouse.src.model.crazy_house.move_presentation import get_move_representation
+from DeepCrazyhouse.src.domain.util import get_board_position_index, get_row_col
+from DeepCrazyhouse.src.model.crazy_house.output_representation import get_move_planes  # outdated
+from DeepCrazyhouse.src.model.crazy_house.move_presentation import get_move_representation  # outdated
 
 
 def construct_move_from_positions(from_pos, to_pos, promotion=None, drop=None):
@@ -14,9 +14,14 @@ def construct_move_from_positions(from_pos, to_pos, promotion=None, drop=None):
 
 def assert_round_trip(test_case: unittest.TestCase, board: CrazyhouseBoard, move, comment=""):
     print(f"Asserting round-trip ({comment})")
-    rt_move = get_move_representation(board, get_move_planes(move), is_white_to_move=True)
+    rt_move = get_move_representation(board, get_move_planes(move), is_white_to_move=True)  # outdated
     print(f"Given {move} -> {rt_move}")
     test_case.assertTrue(move == rt_move, "round trip was not successful")
+
+
+def construct_move_and_assert(test_case, pos1, pos2, comment, promotion=None, drop=None):
+    move = construct_move_from_positions(pos1, pos2, promotion=promotion, drop=drop)
+    assert_round_trip(test_case, CrazyhouseBoard(), move, comment=comment)
 
 
 class MovePlaneRoundTripTests(unittest.TestCase):
@@ -40,55 +45,22 @@ class MovePlaneRoundTripTests(unittest.TestCase):
             self.assertEqual(rt_col, col)
 
     def test_move_planes_round_trip_given_pawn_move_expect_round_trip(self):
-        board = CrazyhouseBoard()
-
-        move = construct_move_from_positions([1, 4], [2, 4])
-        assert_round_trip(self, board, move, comment="pawn move")
+        construct_move_and_assert(self, [1, 4], [2, 4], "pawn move")
 
     def test_move_planes_round_trip_given_knight_move_expect_round_trip(self):
-        board = CrazyhouseBoard()
-
-        def test_move_planes_round_trip_given_knight_move_expect_round_trip(self):
-            board = CrazyhouseBoard()
-
-            move = construct_move_from_positions([4, 4], [2, 3])
-            assert_round_trip(self, board, move, comment="knight move 0")  # A
-
-            move = construct_move_from_positions([4, 4], [3, 2])
-            assert_round_trip(self, board, move, comment="knight move 1")  # B
-
-            move = construct_move_from_positions([4, 4], [2, 5])
-            assert_round_trip(self, board, move, comment="knight move 2")  # C
-
-            move = construct_move_from_positions([4, 4], [3, 6])
-            assert_round_trip(self, board, move, comment="knight move 3")  # D
-
-            move = construct_move_from_positions([4, 4], [6, 3])
-            assert_round_trip(self, board, move, comment="knight move 4")  # E
-
-            move = construct_move_from_positions([4, 4], [5, 2])
-            assert_round_trip(self, board, move, comment="knight move 5")  # F
-
-            move = construct_move_from_positions([4, 4], [6, 5])
-            assert_round_trip(self, board, move, comment="knight move 2")  # G
-
-            move = construct_move_from_positions([4, 4], [5, 6])
-            assert_round_trip(self, board, move, comment="knight move 2")  # H
+        construct_move_and_assert(self, [4, 4], [2, 3], "knight move 0")  # A
+        construct_move_and_assert(self, [4, 4], [3, 2], "knight move 1")  # B
+        construct_move_and_assert(self, [4, 4], [2, 5], "knight move 2")  # C
+        construct_move_and_assert(self, [4, 4], [5, 2], "knight move 3")  # D
+        construct_move_and_assert(self, [4, 4], [3, 6], "knight move 4")  # E
+        construct_move_and_assert(self, [4, 4], [6, 3], "knight move 5")  # F
+        construct_move_and_assert(self, [4, 4], [6, 5], "knight move 6")  # G
+        construct_move_and_assert(self, [4, 4], [5, 6], "knight move 7")  # h
 
     def test_move_planes_round_trip_given_promotion_expect_round_trip(self):
-        board = CrazyhouseBoard()
-
-        move = construct_move_from_positions([6, 4], [7, 4], promotion=3)
-        assert_round_trip(self, board, move, comment="under promotion (straight)")
-
-        move = construct_move_from_positions([6, 4], [7, 3], promotion=3)
-        assert_round_trip(self, board, move, comment="under promotion (left)")
-
-        move = construct_move_from_positions([6, 4], [7, 5], promotion=3)
-        assert_round_trip(self, board, move, comment="under promotion (right)")
+        construct_move_and_assert(self, [6, 4], [7, 4], "under promotion (straight)", promotion=3)
+        construct_move_and_assert(self, [6, 4], [7, 3], "under promotion (left)", promotion=3)
+        construct_move_and_assert(self, [6, 4], [7, 5], "under promotion (right)", promotion=3)
 
     def test_move_planes_round_trip_given_drop_expect_round_trip(self):
-        board = CrazyhouseBoard()
-
-        move = construct_move_from_positions([5, 4], [5, 4], drop=2)
-        assert_round_trip(self, board, move, comment="dropping (bishop)")
+        construct_move_and_assert(self, [5, 4], [5, 4], "dropping (bishop)", drop=2)
