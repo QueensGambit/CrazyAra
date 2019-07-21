589370737118c2e954cc746d98d4e0e575578269
QueensGambit <curry-berry@freenet.de>
2019-07-21 02:25:35 +0200

    updated license header of source files

--- src/agents/agent.h
+++ src/agents/agent.h
@@ -1,13 +1,23 @@
 /*
- * CrazyAra, a deep learning chess variant engine
- * Copyright (C) 2018 Johannes Czech, Moritz Willig, Alena Beyer
- * Copyright (C) 2019 Johannes Czech
- *
- * CrazyAra is free software: You can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
+  CrazyAra, a deep learning chess variant engine
+  Copyright (C) 2018  Johannes Czech, Moritz Willig, Alena Beyer
+  Copyright (C) 2019  Johannes Czech
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/*
  * @file: agent.h
  * Created on 17.06.2019
  * @author: queensgambit
