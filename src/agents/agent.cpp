37e3e5f420075b0bd2e43197fe51d3263c548ef1
QueensGambit <curry-berry@freenet.de>
2019-09-03 19:37:20 +0200

    disabled hardcoded opening book

--- src/agents/agent.cpp
+++ src/agents/agent.cpp
@@ -85,15 +85,16 @@ void Agent::perform_action(Board *pos, SearchLimits* searchLimits, EvalInfo& eva
     set_best_move(evalInfo, pos->total_move_cout());
     cout << evalInfo << endl;
     cout << pos->fen() << endl;
-    if (string(pos->fen()) == string("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR[] b KQkq - 0 1")) {
-        cout << "info string sample move from opening book" << endl;
-        string replies[] = {"e7e5", "e7e5", "e7e5" , "g8f6", "g8f6", "g8f6", "e7e6", "e7e6", "d7d5", "b8c6"};
-        auto seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
-        srand(unsigned(int(seed)));
-        cout << "bestmove " << replies[seed % 10] << endl;
-    }
-    else {
+    // TODO: Enable opening books in a more proper way
+//    if (string(pos->fen()) == string("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR[] b KQkq - 0 1")) {
+//        cout << "info string sample move from opening book" << endl;
+//        string replies[] = {"e7e5", "e7e5", "e7e5" , "g8f6", "g8f6", "g8f6", "e7e6", "e7e6", "d7d5", "b8c6"};
+//        auto seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+//        srand(unsigned(int(seed)));
+//        cout << "bestmove " << replies[seed % 10] << endl;
+//    }
+//    else {
         cout << "bestmove " << UCI::move(evalInfo.bestMove, pos->is_chess960()) << endl;
-    }
+//    }
 }
 
