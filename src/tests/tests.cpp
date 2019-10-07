d78805e6c123e6fb3dcd44ec81d14ae39e052afb
QueensGambit <curry-berry@freenet.de>
2019-10-07 23:58:30 +0200

    added test for AntiChess starting position

--- src/tests/tests.cpp
+++ src/tests/tests.cpp
@@ -28,7 +28,11 @@
 #ifdef BUILD_TESTS
 #include <iostream>
 #include "catch.hpp"
-#include "../sfutil.h"
+#include "../util/sfutil.h"
+#include "../domain/variants.h"
+#include "thread.h"
+#include "../domain/crazyhouse/constants.h"
+#include "../domain/crazyhouse/inputrepresentation.h"
 using namespace Catch::literals;
 using namespace std;
 
@@ -43,5 +47,36 @@ TEST_CASE("En-passent moves") {
         }
         REQUIRE(returnVal == true);
     }
+}
+
+TEST_CASE("Anti-Chess StartFEN"){
+    Bitboards::init();
+    Position::init();
+    Bitbases::init();
+
+    Board pos;
+    string token, cmd;
+    auto uiThread = make_shared<Thread>(0);
+
+    float *inputPlanes = new float[NB_VALUES_TOTAL];
+
+    StateInfo* newState = new StateInfo;
+    pos.set(StartFENs[ANTI_VARIANT], false, ANTI_VARIANT, newState, uiThread.get());
+    board_to_planes(&pos, 0, false, inputPlanes);
+    size_t sum = 0;
+    float max_num = 0;
+    int key = 0;
+    for (size_t i = 0; i < 3000; ++i) {
+        const float val = inputPlanes[i];
+        sum += val;
+        if (inputPlanes[i] > max_num) {
+            max_num = val;
+        }
+        key += i * val;
+    }
+    REQUIRE(NB_VALUES_TOTAL == 3008);
+    REQUIRE(int(max_num) == 1);
+    REQUIRE(int(sum) == 224);
+    REQUIRE(int(key) == 417296);
 }
 #endif
