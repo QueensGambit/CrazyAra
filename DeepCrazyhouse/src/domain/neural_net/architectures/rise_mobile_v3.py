c569dacfd57d0621de4d9d044a29992a2928f0d9
QueensGambit <curry-berry@freenet.de>
2020-03-10 18:28:51 +0100

    added cbam into rise_mobile_v3.py

--- DeepCrazyhouse/src/domain/neural_net/architectures/rise_mobile_v3.py
+++ DeepCrazyhouse/src/domain/neural_net/architectures/rise_mobile_v3.py
@@ -20,19 +20,26 @@ Influenced by the following papers:
     * MobileNetV3: Searching for MobileNetV3,
     Andrew Howard, Mark Sandler, Grace Chu, Liang-Chieh Chen, Bo Chen, Mingxing Tan, Weijun Wang, Yukun Zhu, Ruoming Pang, Vijay Vasudevan, Quoc V. Le, Hartwig Adam.
     https://arxiv.org/abs/1905.02244
+    * Convolutional Block Attention Module (CBAM),
+    Sanghyun Woo, Jongchan Park, Joon-Young Lee, In So Kweon
+    https://arxiv.org/pdf/1807.06521.pdf
 """
 import mxnet as mx
 from DeepCrazyhouse.src.domain.neural_net.architectures.builder_util_symbol import get_act, channel_squeeze_excitation, \
-    mix_conv, get_stem, value_head, policy_head
+    mix_conv, get_stem, value_head, policy_head, convolution_block_attention_module
 
 
-def preact_residual_dmixconv_block(data, channels, channels_operating, name, kernels=None, act_type='relu', use_se=True):
+def preact_residual_dmixconv_block(data, channels, channels_operating, name, kernels=None, act_type='relu', use_se=True,
+                                   se_ratio=4, se_type="se"):
     """
     Returns a residual block without any max pooling operation
     :param data: Input data
     :param channels: Number of filters for all CNN-layers
     :param name: Name for the residual block
     :param act_type: Activation function to use
+    :param se_ratio: Squeeze excitation ratio
+    :param use_se: Boolean if a squeeze excitation module will be used
+    :param se_type: Squeeze excitation module type. Available "se", "cbam"
     :return: symbol
     """
     bn1 = mx.sym.BatchNorm(data=data, name=name + '_bn1')
@@ -44,8 +51,16 @@ def preact_residual_dmixconv_block(data, channels, channels_operating, name, ker
     bn3 = mx.sym.BatchNorm(data=conv2, name=name + '_bn3')
     out = get_act(data=bn3, act_type=act_type, name=name + '_act2')
     if use_se:
-       out = channel_squeeze_excitation(out, channels_operating, name=name + '_se', ratio=4, act_type=act_type,
-                                        use_hard_sigmoid=True)
+        if se_type == "se":
+           out = channel_squeeze_excitation(out, channels_operating, name=name + '_se', ratio=se_ratio, act_type=act_type,
+                                            use_hard_sigmoid=True)
+        elif se_type == "cbam":
+            out = convolution_block_attention_module(out, channels_operating, name=name + '_se', ratio=se_ratio,
+                                                     act_type=act_type,
+                                                     use_hard_sigmoid=True)
+        else:
+            raise Exception(f'Unsupported se_type "{se_type}"')
+
     out = mx.sym.Convolution(data=out, num_filter=channels, kernel=(1, 1),
                                pad=(0, 0), no_bias=True, name=name + '_conv3')
     out_sum = mx.sym.broadcast_add(data, out, name=name + '_add')
@@ -56,7 +71,8 @@ def preact_residual_dmixconv_block(data, channels, channels_operating, name, ker
 def rise_mobile_v3_symbol(channels=256, channels_operating_init=128, channel_expansion=64, act_type='relu',
                           channels_value_head=8, channels_policy_head=81, value_fc_size=256, dropout_rate=0.15,
                           grad_scale_value=0.01, grad_scale_policy=0.99,
-                          select_policy_from_plane=True, use_se=None, kernels=None, n_labels=4992):
+                          select_policy_from_plane=True, use_se=None, kernels=None, n_labels=4992, se_ratio=4,
+                          se_type="se"):
     """
     RISEv3 architecture
     :param channels: Main number of channels
@@ -71,11 +87,21 @@ def rise_mobile_v3_symbol(channels=256, channels_operating_init=128, channel_exp
                             (0.01 is recommended for supervised learning with little data)
     :param grad_scale_policy: Constant scalar which the gradient for the policy outputs are being scaled width.
     :param select_policy_from_plane: True, if policy head type shall be used
+    :param kernels: List of kernel sizes used for the residual blocks. The length of the list corresponds to the number
+    of residual blocks.
     :param use_se: Indicates if a squeeze excitation layer shall be used
-    :param res_blocks: Number of residual blocks
     :param n_labels: Number of policy target labels (used for select_policy_from_plane=False)
+    :param se_ratio: Reduction ration used in the squeeze excitation module
+    :param se_type: Type of squeeze exciation module to use. Available types:
+    - "se": Squeeze excitation block - Hu et al. - https://arxiv.org/abs/1709.01507
+    - "cbam": Convolutional Block Attention Module (CBAM) - Woo et al. - https://arxiv.org/pdf/1807.06521.pdf
     :return: symbol
     """
+
+    se_types = ["se", "cbam"]
+    if se_type not in se_types:
+        raise Exception(f"Unavailable se_type: {se_type}. Available se_types include {se_types}")
+
     # get the input data
     data = mx.sym.Variable(name='data')
 
@@ -90,7 +116,8 @@ def rise_mobile_v3_symbol(channels=256, channels_operating_init=128, channel_exp
 
         cur_use_se = use_se[idx]
         data = preact_residual_dmixconv_block(data=data, channels=channels, channels_operating=cur_channels,
-                                              kernels=cur_kernels, name='dconv_%d' % idx, use_se=cur_use_se)
+                                              kernels=cur_kernels, name='dconv_%d' % idx, use_se=cur_use_se,
+                                              se_ratio=se_ratio, se_type=se_type)
         cur_channels += channel_expansion
 
     data = mx.sym.BatchNorm(data=data, name='stem_bn1')
