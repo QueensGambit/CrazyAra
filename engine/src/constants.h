cea35b213cbf93859573f3b33ac1dfe113529b9c
QueensGambit <curry-berry@freenet.de>
2020-10-23 19:09:04 +0200

    enabled Enhance_Checks UCI option

--- engine/src/constants.h
+++ engine/src/constants.h
@@ -53,7 +53,7 @@ const string engineName = "LichessAra";
 const string engineName = "ClassicAra";
 #endif
 
-const string engineVersion = "0.8.1";
+const string engineVersion = "0.8.2";
 const string engineAuthors = "Johannes Czech, Moritz Willig, Alena Beyer et al.";
 
 // Define the board size
