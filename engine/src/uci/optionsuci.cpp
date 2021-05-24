QueensGambit/CrazyAra

commit d18bdf03e9f76d3b079beff7b4024513380f8be9

Chess960 support (#110)
* Adding printouts to improve readability of logs

* Added tests for all lichess variants

* For RL: Explicitly set `model_contender_dir` by rl_loop.py

* Update nn_index logic. now it is possible to use --nn_update_idx again, when starting a gpu. From now on, it only needs to be passed to the trainer gpu.

* Deleting multiprocessing import in rl_loop as it is not needed

* Support different input names of the currently supported variants

* Readded multiprocessing

* Keep logs of every training (not only the last one)

* Create logs dir after the old one has been moved. Change the point when logs get moved, so the renaming process contains the current nn-update-index

* Implemented support for chess960 and logic for other 960 methods.

* Note that RL only works with gluon atm

* Delete gameidx and gamePGNSelfplay if it exists

* Add Timout_MS to rl_config.py

* Deleted duplicate action_to_uci method.

* Using "open in writing mode" to delete  content RL files in Selfplay.

* Seperate function to handle uci_variant names. Fixed a bug, where the variant was not set correctly for some variants.

Co-authored-by: maxalexger <saaQQMBFCY8Putb>

engine/src/uci/optionsuci.cpp
@@ -34,6 +34,7 @@
 #include <cstring>
 #include "customlogger.h"
 #include "syzygy/tbprobe.h"
+#include "../util/communication.h"
 
 using namespace std;
 
@@ -140,7 +141,7 @@ void OptionsUCI::init(OptionsMap &o)
     o["Use_TensorRT"]                  << Option(true);
 #endif
 #ifdef SUPPORT960
-    o["UCI_Chess960"]                  << Option(true);
+    o["UCI_Chess960"]                  << Option(false);
 #endif
     o["Search_Type"]                   << Option("mcgs", {"mcgs", "mcts"});
 #ifdef USE_RL
@@ -217,26 +218,59 @@ void OptionsUCI::setoption(istringstream &is, Variant& variant, StateObj& state)
         }
 #endif
         Options[name] = value;
-        cout << "info string Updated option " << givenName << " to " << value << endl;
-        if (name == "uci_variant") {
+        if (name != "uci_variant") {
+            cout << "info string Updated option " << givenName << " to " << value << endl;
+        } else {
 #ifdef XIANGQI
             // Workaround. Fairy-Stockfish does not use an enum for variants
             cout << "info string variant " << "Xiangqi" << " startpos " << "rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR w - - 0 1" << endl;
 #else
-            variant = UCI::variant_from_name(value);
-            cout << "info string variant " << (string)Options["UCI_Variant"] << " startpos " << StartFENs[variant] << endl;
-            state.set(StartFENs[variant], Options["UCI_Chess960"], variant);
-#endif
+            bool is960 = false;
+            string uci_variant = check_uci_variant_input(value, &is960);
+            Options["UCI_Variant"] << Option(uci_variant.c_str());
+            info_string("Updated option " + givenName + " to " + uci_variant);
+#ifdef SUPPORT960
+            if (Options["UCI_Chess960"] != is960) {
+                Options["UCI_Chess960"] << Option(is960);
+                info_string("Updated option UCI_Chess960 to " + (string) Options["UCI_Chess960"]);
+            }
+#endif // SUPPORT960
+            variant = UCI::variant_from_name(uci_variant);
+            state.init(variant, is960);
 
-#ifdef MODE_LICHESS // Set model path for new variant; just in case set model_contender as well
-            Options["Model_Directory"] << ((string) "model" + "/" + string(Options["UCI_Variant"])).c_str();
-            Options["Model_Directory_Contender"] << Option(((string) "model_contender/" + string(Options["UCI_Variant"])).c_str());
+            cout << is960 << endl;
+
+            string suffix_960 = (is960) ? "960" : "";
+            Options["Model_Directory"] << Option(("model/" + (string)Options["UCI_Variant"] + suffix_960).c_str());
+            Options["Model_Directory_Contender"] << Option(("model_contender/" + (string)Options["UCI_Variant"] + suffix_960).c_str());
+            info_string("info string variant " + (string)Options["UCI_Variant"] + suffix_960 + " startpos " + state.fen());
 #endif
         }
     }
     else {
         cout << "info string Given option " << name << " does not exist " << endl;
     }
+}
+
+string OptionsUCI::check_uci_variant_input(const string &value, bool *is960) {
+    // default value of is960 == false
+#ifdef SUPPORT960
+    // we only want 960 for chess atm
+    if (value == "fischerandom" || value == "chess960"
+        || (((value == "chess") || (value == "standard")) && Options["UCI_Chess960"])) {
+        *is960 = true;
+        return "chess";
+    }
+#endif // SUPPORT960
+    if (value == "standard") {
+       return "chess";
+    }
+#ifdef MODE_LICHESS
+    if (value == "antichess" || value == "losers") {
+        return "giveaway";
+    }
+    return value;
+#endif // MODE_LICHESS
 }
 
 void OptionsUCI::init_new_search(SearchLimits& searchLimits, OptionsMap &options)

DeepCrazyhouse/configs/rl_config.py
@@ -59,6 +59,7 @@ class UCIConfig:
     Simulations: int = 3200
     SyzygyPath: str = f''
     Temperature_Moves: int = 15  # CZ: 500
+    Timeout_MS: int = 0
 
 
 @dataclass

DeepCrazyhouse/configs/train_config_template.py
@@ -100,7 +100,9 @@ class TrainConfig:
     # total of training iterations
     total_it: int = None
 
-    use_mxnet_style: bool = True  # Decide between mxnet and gluon style for training
+    # Decide between mxnet and gluon style for training
+    # Reinforcement Learning only works with gluon (== False) atm
+    use_mxnet_style: bool = True
 
     # loads a previous checkpoint if the loss increased significantly
     use_spike_recovery: bool = True

engine/CMakeLists.txt
@@ -42,6 +42,7 @@ if (MODE_LICHESS)
     add_definitions(-DATOMIC)
     add_definitions(-DHORDE)
     add_definitions(-DRACE)
+    add_definitions(-DSUPPORT960)
     add_definitions(-DMCTS_TB_SUPPORT)
 endif()
 

engine/src/environments/chess_related/boardstate.cpp
@@ -28,6 +28,8 @@
 #include "inputrepresentation.h"
 #include "syzygy/tbprobe.h"
 #include "uci/variants.h"
+#include "chess960position.h"
+#include "../util/communication.h"
 
 action_idx_map OutputRepresentation::MV_LOOKUP = {};
 action_idx_map OutputRepresentation::MV_LOOKUP_MIRRORED = {};
@@ -254,7 +256,14 @@ BoardState* BoardState::clone() const
 void BoardState::init(int variant, bool is960)
 {
     states = StateListPtr(new std::deque<StateInfo>(1));
-    board.set(StartFENs[variant], is960, Variant(variant), &states->back(), nullptr);
+    string start_fen = StartFENs[variant];
+    if(is960 && variant == CHESS_VARIANT) {
+        start_fen = chess960fen();
+    } else if (is960) {
+        info_string("960 has not yet been implemented for" + variants[variant]);
+        info_string("Using standard starting position instead.");
+    }
+    board.set(start_fen, is960, Variant(variant), &states->back(), nullptr);
 }
 
 #endif

engine/src/environments/chess_related/chess960position.h
@@ -1,0 +1,88 @@
+/*
+  CrazyAra, a deep learning chess variant engine
+  Copyright (C) 2018       Johannes Czech, Moritz Willig, Alena Beyer
+  Copyright (C) 2019-2020  Johannes Czech
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/*
+ * @file: chess960position.h
+ * Created on 25.04.2020
+ * @author: queensgambit
+ *
+ * http://rosettacode.org/wiki/Generate_Chess960_starting_position
+ */
+
+#ifndef CHESS960POSITION_H
+#define CHESS960POSITION_H
+
+class Chess960Position
+{
+public:
+    Chess960Position();
+};
+
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <cstring>
+using namespace std;
+
+namespace
+{
+    void placeRandomly(char* p, char c)
+    {
+        int loc = rand() % 8;
+        if (!p[loc])
+            p[loc] = c;
+        else
+            placeRandomly(p, c);    // try again
+    }
+    int placeFirst(char* p, char c, int loc = 0)
+    {
+        while (p[loc]) ++loc;
+        p[loc] = c;
+        return loc;
+    }
+
+    string startPos()
+    {
+        char p[8]; memset( p, 0, 8 );
+
+        // bishops on opposite color
+        p[2 * (rand() % 4)] = 'B';
+        p[2 * (rand() % 4) + 1] = 'B';
+
+        // queen knight knight, anywhere
+        for (char c : "QNN")
+            placeRandomly(p, c);
+
+        // rook king rook, in that order
+        placeFirst(p, 'R', placeFirst(p, 'K', placeFirst(p, 'R')));
+
+        return string(p, 8);
+    }
+
+    string chess960fen()
+    {
+        string firstRank = startPos();
+        string lastRank = string(firstRank);
+        std::transform(firstRank.begin(), firstRank.end(), firstRank.begin(), ::tolower);
+        const string fen = firstRank + "/pppppppp/8/8/8/8/PPPPPPPP/" + lastRank +  " w KQkq - 0 1";
+        return fen;
+    }
+}   // leave local
+
+#endif // CHESS960POSITION_H

engine/src/rl/binaryio.py
@@ -48,6 +48,7 @@ class BinaryIO:
         folder with the compressed games, as well as 'games.pgn' and 'gameIdx.txt'.
         :return:
         """
+        logging.info(f'Generating games ...')
         # if 0, binary plays Selfplay_Number_Chunks * Selfplay_Chunk_Size games
         self.proc.stdin.write(b"selfplay 0\n")
         self.proc.stdin.flush()
@@ -85,6 +86,7 @@ class BinaryIO:
         Tells the binary to load the network and waits until it's finished.
         :return:
         """
+        logging.info(f'Loading network & creating backend files ...', )
         self.proc.stdin.write(b"isready\n")
         self.proc.stdin.flush()
         self.read_output(b"readyok\n", check_error=True)
@@ -124,7 +126,7 @@ class BinaryIO:
                 return True
 
     def set_uci_options(self, uci_variant: str, context: str, device_id: str, precision: str,
-                        model_dir: str, is_arena: bool = False):
+                        model_dir: str, model_contender_dir: str, is_arena: bool = False):
         """
         Sets UCI options of the binary.
         :param uci_variant: The UCI variant that shall be trained.
@@ -132,10 +134,12 @@ class BinaryIO:
         :param device_id: The id of the device we are using.
         :param precision: The precision of calculations.
         :param model_dir: The path to the model.
+        :param model_contender_dir: Directory where the model contender dir will be saved.
         :param is_arena: Applies setting for the arena comparison
         :return:
         """
         self._set_uci_param(f'Model_Directory', model_dir)
+        self._set_uci_param(f'Model_Contender_Directory', model_contender_dir)
         self._set_uci_param(f'UCI_Variant', uci_variant)
         self._set_uci_param(f'Context', context)
         self._set_uci_param(f'First_Device_ID', device_id)

engine/src/rl/fileio.py
@@ -95,6 +95,8 @@ class FileIO:
         self.val_dir_archive = binary_dir + "export/archive/val/" + variant_suffix
         self.model_contender_dir = binary_dir + "model_contender/" + variant_suffix
         self.model_dir_archive = binary_dir + "export/archive/model/" + variant_suffix
+        self.logs_dir_archive = binary_dir + "export/logs/" + variant_suffix
+        self.logs_dir = binary_dir + "logs"
 
         self._create_directories()
 
@@ -109,7 +111,7 @@ class FileIO:
         Creates directories in the binary folder which will be used during RL
         :return:
         """
-        create_dir(self.binary_dir+"logs")
+        create_dir(self.logs_dir)
         create_dir(self.weight_dir)
         create_dir(self.export_dir_gen_data)
         create_dir(self.train_dir)
@@ -118,6 +120,7 @@ class FileIO:
         create_dir(self.val_dir_archive)
         create_dir(self.model_contender_dir)
         create_dir(self.model_dir_archive)
+        create_dir(self.logs_dir_archive)
 
     def _include_data_from_replay_memory(self, nb_files: int, fraction_for_selection: float):
         """
@@ -250,6 +253,15 @@ class FileIO:
                       "gameIdx_" + device_name + ".txt"]
         for file_name in file_names:
             os.rename(self.binary_dir + file_name, export_dir + file_name)
+
+    def move_training_logs(self, nn_update_index):
+        """
+        Rename logs and move it from /logs to /export/logs/
+        """
+        time_stamp = datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d-%H-%M-%S")
+        dir_name = f'logs-{self.uci_variant}-update{nn_update_index}-{time_stamp}'
+        os.rename(self.logs_dir, os.path.join(self.logs_dir_archive, dir_name))
+        create_dir(self.logs_dir)
 
     def prepare_data_for_training(self, rm_nb_files, rm_fraction_for_selection):
         """

commit 7329a1eb3add45434133e9e104de979b0d551273

fix bug introduced in #110 when building in MODE_CRAZYHOUSE

engine/src/uci/optionsuci.cpp
@@ -226,16 +226,16 @@ void OptionsUCI::setoption(istringstream &is, Variant& variant, StateObj& state)
             info_string_important("variant Xiangqi startpos rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR w - - 0 1");
 #else
             bool is960 = false;
-            string uci_variant = check_uci_variant_input(value, &is960);
-            Options["UCI_Variant"] << Option(uci_variant.c_str());
-            info_string_important("Updated option", givenName, "to", uci_variant);
+            string uciVariant = check_uci_variant_input(value, &is960);
+            Options["UCI_Variant"] << Option(uciVariant.c_str());
+            info_string_important("Updated option", givenName, "to", uciVariant);
 #ifdef SUPPORT960
             if (Options["UCI_Chess960"] != is960) {
                 Options["UCI_Chess960"] << Option(is960);
                 info_string("Updated option UCI_Chess960 to", (string)Options["UCI_Chess960"]);
             }
 #endif // SUPPORT960
-            variant = UCI::variant_from_name(uci_variant);
+            variant = UCI::variant_from_name(uciVariant);
             state.init(variant, is960);
 
             string suffix_960 = (is960) ? "960" : "";
@@ -269,6 +269,8 @@ string OptionsUCI::check_uci_variant_input(const string &value, bool *is960) {
     }
     return value;
 #endif // MODE_LICHESS
+    // MODE_CRAZYHOUSE or others (keep value as is)
+    return value;
 }
 
 void OptionsUCI::init_new_search(SearchLimits& searchLimits, OptionsMap &options)