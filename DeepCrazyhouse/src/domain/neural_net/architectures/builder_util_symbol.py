8007fa0bc20d25987f50ca71f914b8acb8b4e45e
QueensGambit <curry-berry@freenet.de>
2021-06-01 00:17:57 +0200

    removed wld_softmax (#123)

--- DeepCrazyhouse/src/domain/neural_net/architectures/builder_util_symbol.py
+++ DeepCrazyhouse/src/domain/neural_net/architectures/builder_util_symbol.py
@@ -149,12 +149,11 @@ def value_head(data, channels_value_head=4, value_kernelsize=1, act_type='relu',
 
     value_main_features = get_act(data=value_out, act_type=act_type, name='value_act1')
 
-    wdl_out = None
     plys_to_end_out = None
-    wdl_softmax = None
+    wdl_out = None
     if use_wdl:
         wdl_out = mx.sym.FullyConnected(data=value_main_features, num_hidden=3, name=main_config["wdl_output"])
-        wdl_softmax = mx.sym.SoftmaxOutput(data=wdl_out, name='wdl', grad_scale=grad_scale_wdl)
+        wdl_out = mx.sym.SoftmaxOutput(data=wdl_out, name='wdl', grad_scale=grad_scale_wdl)
     if use_plys_to_end:
         plys_to_end_out = mx.sym.FullyConnected(data=value_main_features, num_hidden=1, name='value_plys_to_end_fc')
         plys_to_end_out = get_act(data=plys_to_end_out, act_type='sigmoid', name=main_config["plys_to_end_output"])
@@ -162,21 +161,21 @@ def value_head(data, channels_value_head=4, value_kernelsize=1, act_type='relu',
 
     if use_wdl and use_plys_to_end:
         if use_mlp_wdl_ply:
-            value_out = mx.sym.Concat(wdl_softmax, plys_to_end_out, dim=1, name='value_concat_0')
+            value_out = mx.sym.Concat(wdl_out, plys_to_end_out, dim=1, name='value_concat_0')
             value_out = mx.sym.FullyConnected(data=value_out, num_hidden=8, name='value_wdl_ply_fc')
             value_out = get_act(data=value_out, act_type=act_type, name='value_wdl_ply_act')
             value_out = mx.sym.FullyConnected(data=value_out, num_hidden=1, name='value_fc1')
             value_out = get_act(data=value_out, act_type='tanh', name=main_config["value_output"])
             value_out = mx.sym.LinearRegressionOutput(data=value_out, name='value', grad_scale=1)
         else:  # hard-coded
-            (loss_out, _, win_out) = mx.sym.split(wdl_softmax, axis=1, num_outputs=3, name='win_loss_split')
+            (loss_out, _, win_out) = mx.sym.split(wdl_out, axis=1, num_outputs=3, name='win_loss_split')
             value_out = mx.sym.broadcast_add(-loss_out, win_out, name=main_config["value_output"])
             value_out = mx.sym.LinearRegressionOutput(data=value_out, name='value', grad_scale=0)
     else:
         value_out = mx.sym.FullyConnected(data=value_out, num_hidden=1, name='value_fc1')
         value_out = get_act(data=value_out, act_type='tanh', name=main_config["value_output"])
         value_out = mx.sym.LinearRegressionOutput(data=value_out, name='value', grad_scale=grad_scale_value)
-    return value_out, wdl_out, wdl_softmax, plys_to_end_out
+    return value_out, wdl_out, plys_to_end_out
 
 
 def policy_head(data, channels, act_type, channels_policy_head, select_policy_from_plane, n_labels,
