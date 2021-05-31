eb19e7737e37335293c095e4db6b5253562fc958
QueensGambit <curry-berry@freenet.de>
2021-05-31 11:36:39 +0200

    added searchSetting useTwoFoldRepetition

--- engine/src/agents/config/searchsettings.h
+++ engine/src/agents/config/searchsettings.h
@@ -68,6 +68,8 @@ struct SearchSettings
     bool reuseTree;
     // If true, then the MCTS solver for terminals and tablebases will be active
     bool mctsSolver;
+    // If true, then a two fold repetition will be seen as a draw during search
+    bool useTwoFoldRepetition;
     SearchSettings();
 
 };
