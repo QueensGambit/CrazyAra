a075dd99cec897aabfc37cd26ee92834343cecc3
QueensGambit <curry-berry@freenet.de>
2021-07-01 19:50:49 +0200

    updated download_png_files.sh + add unzipping directly afterwards + added check if pgn has already been downloaded

--- DeepCrazyhouse/src/preprocessing/download_pgns/download_png_files.sh
+++ DeepCrazyhouse/src/preprocessing/download_pgns/download_png_files.sh
@@ -4,7 +4,22 @@
 file="./pgn_list_lichess_variants.txt"
 while IFS= read -r line
 do
+filename=$(echo $line| cut -d'/' -f 5)
+filepgn=${filename::${#filename}-4}
+
+if [[ -f "${PWD}/${filepgn}" ]]; then
+	echo "Already downloaded $filepgn"
+else
 	printf 'Downloading file %s ...\n' "$line"
-	curl -O "$line"
+if curl --silent -o "${PWD}/${filename}" -L "$line"; then
+     bunzip2 "${filename}"
+     if [[ -f "${PWD}/${filename}" ]]; then
+         echo "Removing file ${filename}"
+         rm -f "${PWD}/${filename}"
+     fi
+else
+     echo "Something went wrong"
+fi
+fi
 done <"$file"
 printf 'Download finished sucessfully!\n'
