a6992ba24bf9b99cde33770dc80224cff0d9a20b
QueensGambit <curry-berry@freenet.de>
2022-06-02 19:06:47 +0200

    Add more training loop information

--- DeepCrazyhouse/src/training/trainer_agent_pytorch.py
+++ DeepCrazyhouse/src/training/trainer_agent_pytorch.py
@@ -11,10 +11,12 @@ https://gitlab.com/jweil/PommerLearn/-/blob/master/pommerlearn/training/train_cn
 
 import random
 import logging
+import numpy as np
 from pathlib import Path
 import torch
 import torch.nn as nn
 from time import time
+import datetime
 from rtpt import RTPT
 from tqdm import tqdm_notebook
 from torch.utils.tensorboard import SummaryWriter
@@ -23,6 +25,7 @@ from torch.utils.data import TensorDataset, DataLoader
 from DeepCrazyhouse.configs.train_config import TrainConfig, TrainObjects
 from DeepCrazyhouse.src.preprocessing.dataset_loader import load_pgn_dataset
 from DeepCrazyhouse.src.training.pytorch.metrics import Metrics
+from DeepCrazyhouse.src.training.trainer_agent_mxnet import prepare_policy, return_metrics_and_stop_training
 
 
 class TrainerAgentPytorch:
@@ -56,6 +59,10 @@ class TrainerAgentPytorch:
         # define a summary writer that logs data and flushes to the file every 5 seconds
         if self.tc.log_metrics_to_tensorboard:
             self.sum_writer = SummaryWriter(log_dir=self.tc.export_dir+"logs", flush_secs=5)
+        # Define the two loss functions
+        self._cross_entropy_loss = nn.CrossEntropyLoss()
+        self._l2_loss = nn.MSELoss()
+
         # Define the optimizer
         self.optimizer = create_optimizer(self._model, self.tc)
 
@@ -113,64 +120,197 @@ class TrainerAgentPytorch:
                                                                                          normalize=self.tc.normalize,
                                                                                          verbose=False,
                                                                                          q_value_ratio=self.tc.q_value_ratio)
+
+        self.yp_train = prepare_policy(y_policy=self.yp_train, select_policy_from_plane=self.tc.select_policy_from_plane,
+                                  sparse_policy_label=self.tc.sparse_policy_label,
+                                  is_policy_from_plane_data=self.tc.is_policy_from_plane_data)
+
+        # update the train_data object
         train_dataset = TensorDataset(torch.Tensor(self.x_train), torch.Tensor(self.yv_train),
                                       torch.Tensor(self.yp_train), torch.Tensor(self.plys_to_end))
-        train_loader = DataLoader(train_dataset, shuffle=True, batch_size=self.tc.batch_size)
-        # training
-        for batch_idx, batch in enumerate(train_loader):
-            self.optimizer.zero_grad()
-            x_train, yv_train, yp_train, plys_to_end = batch
+        train_loader = DataLoader(train_dataset, shuffle=True, batch_size=self.tc.batch_size, num_workers=self.tc.cpu_count)
 
-            x_train = x_train.to(self._ctx)
-            yv_train = yv_train.to(self._ctx)
-            yp_train = yp_train.to(self._ctx)
-            plys_to_end = plys_to_end.to(self._ctx)
+        for _, (data, value_label, policy_label, plys_label) in enumerate(train_loader):
+            data = data.to(self._ctx)
+            value_label = value_label.to(self._ctx)
+            policy_label = policy_label.to(self._ctx)
+            plys_label = plys_label.to(self._ctx)
 
-            combined_loss = self.train_metrics.update(self._model, policy_loss, value_loss, self.tc.val_loss_factor, x_train, yv_train, yp_train)
+            # update a dummy metric to see a proper progress bar
+            #  (the metrics will get evaluated at the end of 100k steps)
+            if self.batch_proc_tmp > 0:
+                self.to.metrics["value_loss"].update(self.old_label, value_out)
+
+            self.old_label = value_label
+
+            value_out, policy_out = self._model(data)
+            policy_out = policy_out.softmax()
+            value_loss = self._l2_loss(value_out, value_label)
+            policy_loss = self._cross_entropy_loss(policy_out, policy_label)
+            # weight the components of the combined loss
+            combined_loss = (
+                    self.tc.val_loss_factor * value_loss + self.tc.policy_loss_factor * policy_loss
+            )
 
             combined_loss.backward()
-            lr, momentum = 0, 0
             for param_group in self.optimizer.param_groups:
-                lr = lr_schedule(local_step)
-                param_group['lr'] = lr
-
-                momentum = momentum_schedule(local_step)
-                param_group['momentum'] = momentum
+                param_group['lr'] = self.to.lr_schedule(self.cur_it)  # update the learning rate
+                param_group['momentum'] = self.to.momentum_schedule(self.cur_it)  # update the momentum
 
             self.optimizer.step()
 
-            if (batches_until_eval is not None and (batch_idx + 1) % batches_until_eval == 0) \
-                    or (batch_idx + 1) == len(train_loader):
-                msg = f' epoch: {epoch}, batch index: {batch_idx + 1}, train value loss: {m_train.value_loss():5f},'\
-                      f' train policy loss: {m_train.policy_loss():5f}, train policy acc: {m_train.policy_acc():5f}'
+            self.cur_it += 1
+            self.batch_proc_tmp += 1
+            # add the graph representation of the network to the tensorboard log file
+            if not self.graph_exported and self.tc.log_metrics_to_tensorboard:
+                self.sum_writer.add_graph(self._model)
+                self.graph_exported = True
 
-                # log last lr and momentum
-                self.sum_writer.add_scalar('Hyperparameter/Learning Rate', lr, global_step)
-                self.sum_writer.add_scalar('Hyperparameter/Momentum', momentum, global_step)
+            if self.batch_proc_tmp >= self.tc.batch_steps:  # show metrics every thousands steps
+                # log the current learning rate
+                # update batch_proc_tmp counter by subtracting the batch_steps
+                batch_proc_tmp = self.batch_proc_tmp - self.tc.batch_steps
+                ms_step = ((time() - self.t_s_steps) / self.tc.batch_steps) * 1000  # measure elapsed time
+                # update the counters
+                self.k_steps += 1
+                self.patience_cnt += 1
+                logging.info("Step %dK/%dK - %dms/step", self.k_steps, self.k_steps_end, ms_step)
+                logging.info("-------------------------")
+                logging.debug("Iteration %d/%d", self.cur_it, self.tc.total_it)
+                logging.debug("lr: %.7f - momentum: %.7f", self.to.lr_schedule(self.cur_it), self.to.momentum_schedule(self.cur_it))
+                train_metric_values = evaluate_metrics(
+                    self.to.metrics,
+                    train_data,
+                    self._model,
+                    nb_batches=10,  # 25,
+                    ctx=self._ctx,
+                    sparse_policy_label=self.tc.sparse_policy_label,
+                    apply_select_policy_from_plane=self.tc.select_policy_from_plane and not self.tc.is_policy_from_plane_data
+                )
+                val_metric_values = evaluate_metrics(
+                    self.to.metrics,
+                    self._val_data,
+                    self._model,
+                    nb_batches=None,
+                    ctx=self._ctx,
+                    sparse_policy_label=self.tc.sparse_policy_label,
+                    apply_select_policy_from_plane=self.tc.select_policy_from_plane and not self.tc.is_policy_from_plane_data
+                )
+                if self.use_rtpt:
+                    # update process title according to loss
+                    self.rtpt.step(subtitle=f"loss={val_metric_values['loss']:2.2f}")
+                if self.tc.use_spike_recovery and (
+                        old_val_loss * self.tc.spike_thresh < val_metric_values["loss"]
+                        or np.isnan(val_metric_values["loss"])
+                ):  # check for spikes
+                    self.nb_spikes += 1
+                    logging.warning(
+                        "Spike %d/%d occurred - val_loss: %.3f",
+                        self.nb_spikes,
+                        self.tc.max_spikes,
+                        val_metric_values["loss"],
+                    )
+                    if self.nb_spikes >= self.tc.max_spikes:
+                        self.val_loss = val_metric_values["loss"]
+                        self.val_p_acc = val_metric_values["policy_acc"]
+                        logging.debug("The maximum number of spikes has been reached. Stop training.")
+                        # finally stop training because the number of lr drops has been achieved
+                        print()
+                        print(
+                            "Elapsed time for training(hh:mm:ss): "
+                            + str(datetime.timedelta(seconds=round(time() - self.t_s)))
+                        )
 
-                # log aggregated train stats
-                m_train.log_to_tensorboard(writer_train, global_step)
-                m_train.reset()
+                        if self.tc.log_metrics_to_tensorboard:
+                            self.sum_writer.close()
+                        return return_metrics_and_stop_training(self.k_steps, val_metric_values, self.k_steps_best,
+                                                                self.val_metric_values_best)
 
-                if val_loader is not None:
-                    model.eval()
-                    m_val = get_val_loss(model, value_loss_ratio, value_loss, policy_loss, device, val_loader)
+                    logging.debug("Recover to latest checkpoint")
+                    model_path = self.tc.export_dir + "weights/model-%.5f-%.3f-%04d.params" % (
+                        self.val_loss_best,
+                        self.val_p_acc_best,
+                        self.k_steps_best,
+                    )  # Load the best model once again
+                    logging.debug("load current best model:%s", model_path)
+                    self._net.load_parameters(model_path, ctx=self._ctx)
+                    k_steps = self.k_steps_best
+                    logging.debug("k_step is back at %d", self.k_steps_best)
+                    # print the elapsed time
+                    t_delta = time() - self.t_s_steps
+                    print(" - %.ds" % t_delta)
+                    t_s_steps = time()
+                else:
+                    # update the val_loss_value to compare with using spike recovery
+                    old_val_loss = val_metric_values["loss"]
+                    # log the metric values to tensorboard
+                    self._log_metrics(train_metric_values, global_step=self.k_steps, prefix="train_")
+                    self._log_metrics(val_metric_values, global_step=self.k_steps, prefix="val_")
 
-                    m_val.log_to_tensorboard(writer_val, global_step)
-                    msg += f', val value loss: {m_val.value_loss():5f}, val policy loss: {m_val.policy_loss():5f},'\
-                           f' val policy acc: {m_val.policy_acc():5f}'
+                    if self.tc.export_grad_histograms:
+                        grads = []
+                        # logging the gradients of parameters for checking convergence
+                        for _, name in enumerate(self._param_names):
+                            if "bn" not in name and "batch" not in name and name != "policy_flat_plane_idx":
+                                grads.append(self._params[name].grad())
+                                self.sum_writer.add_histogram(
+                                    tag=name, values=grads[-1], global_step=self.k_steps, bins=20
+                                )
 
-                print(msg)
+                    # check if a new checkpoint shall be created
+                    if self.val_loss_best is None or val_metric_values["loss"] < self.val_loss_best:
+                        # update val_loss_best
+                        val_loss_best = val_metric_values["loss"]
+                        val_p_acc_best = val_metric_values["policy_acc"]
+                        val_metric_values_best = val_metric_values
+                        k_steps_best = self.k_steps
 
-            global_step += 1
-            local_step += 1
-            progress.update(1)
+                        if self.tc.export_weights:
+                            prefix = self.tc.export_dir + "weights/model-%.5f-%.3f" \
+                                     % (val_loss_best, val_p_acc_best)
+                            # the export function saves both the architecture and the weights
+                            export_as_script_module(self._model, batch_size, data, "./weights")
+                            print()
+                            logging.info("Saved checkpoint to %s-%04d.params", prefix, k_steps_best)
 
-    progress.close()
+                        patience_cnt = 0  # reset the patience counter
+                    # print the elapsed time
+                    t_delta = time() - self.t_s_steps
+                    print(" - %.ds" % t_delta)
+                    t_s_steps = time()
 
-    self.sum_writer.close()
-    if val_loader is not None:
-        writer_val.close()
+                    # log the samples per second metric to tensorboard
+                    self.sum_writer.add_scalar(
+                        tag="samples_per_second",
+                        scalar_value={"hybrid_sync": data.shape[0] * self.tc.batch_steps / t_delta},
+                        global_step=self.k_steps,
+                    )
+
+                    # log the current learning rate
+                    self.sum_writer.add_scalar(tag="lr", scalar_value=self.to.lr_schedule(self.cur_it), global_step=self.k_steps)
+                    # log the current momentum value
+                    self.sum_writer.add_scalar(
+                        tag="momentum", scalar_value=self.to.momentum_schedule(self.cur_it), global_step=k_steps
+                    )
+
+                    if self.cur_it >= self.tc.total_it:
+
+                        val_loss = val_metric_values["loss"]
+                        val_p_acc = val_metric_values["policy_acc"]
+                        logging.debug("The number of given iterations has been reached")
+                        # finally stop training because the number of lr drops has been achieved
+                        print()
+                        print(
+                            "Elapsed time for training(hh:mm:ss): "
+                            + str(datetime.timedelta(seconds=round(time() - self.t_s)))
+                        )
+
+                        if self.tc.log_metrics_to_tensorboard:
+                            self.sum_writer.close()
+
+                        return return_metrics_and_stop_training(k_steps, val_metric_values, self.k_steps_best,
+                                                                self.val_metric_values_best)
+
 
     # make sure to empty cache
     if torch.cuda.is_available():
@@ -334,3 +474,56 @@ def export_as_script_module(model, batch_size, dummy_input, dir) -> None:
     # serialize script module to file
     traced_script_module.save(str(dir / Path(f"model-bsize-{batch_size}.pt")))
 
+
+def reset_metrics(metrics):
+    """
+    Resets all metric entries in a dictionary object
+    :param metrics:
+    :return:
+    """
+    for metric in metrics.values():
+        metric.reset()
+
+
+def evaluate_metrics(metrics, data_iterator, model, nb_batches, ctx, sparse_policy_label=False,
+                     apply_select_policy_from_plane=True):
+    """
+    Runs inference of the network on a data_iterator object and evaluates the given metrics.
+    The metric results are returned as a dictionary object.
+
+    :param metrics: List of mxnet metrics which must have the
+    names ['value_loss', 'policy_loss', 'value_acc_sign', 'policy_acc']
+    :param data_iterator: Pytorch data iterator object
+    :param model: Pytorch model handle
+    :param nb_batches: Number of batches to evaluate (early stopping).
+     If set to None all batches of the data_iterator will be evaluated
+    :param ctx: Pytorch data context
+    :param sparse_policy_label: Should be set to true if the policy uses one-hot encoded targets
+     (e.g. supervised learning)
+    :param apply_select_policy_from_plane: If true, given policy label is converted to policy map index
+    :return:
+    """
+    reset_metrics(metrics)
+    for i, (data, value_label, policy_label) in enumerate(data_iterator):
+        data = data.to(ctx)
+        value_label = value_label.to(ctx)
+        policy_label = policy_label.to(ctx)
+
+        value_out, policy_out = model(data)
+
+        # update the metrics
+        metrics["value_loss"].update(preds=value_out, labels=value_label)
+        metrics["policy_loss"].update(preds=policy_out.softmax(),
+                                      labels=policy_label)
+        metrics["value_acc_sign"].update(preds=value_out, labels=value_label)
+        metrics["policy_acc"].update(preds=policy_out.argmax(axis=1),
+                                     labels=policy_label)
+        # stop after evaluating x batches (only recommended to use this for the train set evaluation)
+        if nb_batches and i == nb_batches:
+            break
+
+    metric_values = {"loss": 0.01 * metrics["value_loss"].get()[1] + 0.99 * metrics["policy_loss"].get()[1]}
+
+    for metric in metrics.values():
+        metric_values[metric.get()[0]] = metric.get()[1]
+    return metric_values
