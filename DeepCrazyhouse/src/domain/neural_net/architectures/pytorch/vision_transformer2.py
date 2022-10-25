ff24c5b876a319fb172144d64417140fc1425778
QueensGambit <curry-berry@freenet.de>
2022-10-25 19:07:02 +0200

    Update ChessViT Allow typical _ValueHead with flat inputs

--- DeepCrazyhouse/src/domain/neural_net/architectures/pytorch/vision_transformer2.py
+++ DeepCrazyhouse/src/domain/neural_net/architectures/pytorch/vision_transformer2.py
@@ -13,15 +13,19 @@ from torch import nn
 from einops import rearrange, repeat
 from einops.layers.torch import Rearrange
 from vit_pytorch.vit import pair, Transformer
+from DeepCrazyhouse.src.domain.neural_net.architectures.pytorch.builder_util import _ValueHead
 
 input_shape = (52, 8, 8)
 
 
 class ChessViT(nn.Module):
-    def __init__(self, *, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, pool = 'cls', channels = 3, dim_head = 64, dropout = 0., emb_dropout = 0.):
+    def __init__(self, *, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, pool = 'cls', channels = 3, dim_head = 64, dropout = 0., emb_dropout = 0.,
+                 use_wdl=False, use_plys_to_end=False, use_mlp_wdl_ply=False):
         super().__init__()
         image_height, image_width = pair(image_size)
         patch_height, patch_width = pair(patch_size)
+        self.use_wdl = use_wdl
+        self.use_plys_to_end = use_plys_to_end
 
         assert image_height % patch_height == 0 and image_width % patch_width == 0, 'Image dimensions must be divisible by the patch size.'
 
@@ -48,6 +52,9 @@ class ChessViT(nn.Module):
             nn.Linear(dim, 1),
             nn.Tanh()
         )
+        self.value_head = _ValueHead(board_height=image_size, board_width=image_size, channels=0, channels_value_head=0, fc0=256,
+                                     nb_input_channels=256, use_wdl=use_wdl, use_plys_to_end=use_plys_to_end, use_mlp_wdl_ply=use_mlp_wdl_ply,
+                                     use_flat_inputs=True, in_features=dim)
         self.mlp_policy_head = nn.Sequential(
             nn.LayerNorm(dim),
             nn.Linear(dim, num_classes)
@@ -67,4 +74,12 @@ class ChessViT(nn.Module):
         x = x.mean(dim = 1) if self.pool == 'mean' else x[:, 0]
 
         x = self.to_latent(x)
-        return self.mlp_value_head(x), self.mlp_policy_head(x)
+        value_head_out = self.value_head(x)
+        logits = self.mlp_policy_head(x)
+
+        if self.use_plys_to_end and self.use_wdl:
+            value_out, wdl_out, plys_to_end_out = value_head_out
+            auxiliary_out = torch.cat((wdl_out, plys_to_end_out), dim=1)
+            return value_out, logits, auxiliary_out, wdl_out, plys_to_end_out
+        else:
+            return value_head_out, logits
