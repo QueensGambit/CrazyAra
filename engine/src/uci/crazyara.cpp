05499b56bed1b38a51e02bfa00362c76246822fe
tam201197 <tam201197@gmail.com>
2023-05-03 17:05:04 +0200

    delete isready

--- engine/src/uci/crazyara.cpp
+++ engine/src/uci/crazyara.cpp
@@ -95,7 +95,6 @@ void CrazyAra::uci_loop(int argc, char* argv[])
 
     // this is debug vector which can contain uci commands which will be automatically processed when the executable is launched
     vector<string> commands = {
-        "isready"
     };
 
     do {
