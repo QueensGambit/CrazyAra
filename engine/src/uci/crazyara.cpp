b3eae97239e3227e7c08e4a99b1d6fa2999efe0a
tam201197 <tam201197@gmail.com>
2023-05-07 22:11:02 +0200

    fix ucioption

--- engine/src/uci/crazyara.cpp
+++ engine/src/uci/crazyara.cpp
@@ -715,7 +715,7 @@ void CrazyAra::init_search_settings()
     else {
         searchSettings.backupOperator = BACKUP_MAX;
     }
-    searchSettings.weightMinimax = Options["Weight_Minimax"];
+    searchSettings.weightMinimax = Options["Weight_Minimax"] / 100.0f;
     
 }
 
