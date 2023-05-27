0e91772ff16c639d306441bc30ea75e42df5570c
tam201197 <tam201197@gmail.com>
2023-05-27 09:51:36 +0200

    fixed null

--- engine/src/node.cpp
+++ engine/src/node.cpp
@@ -676,7 +676,7 @@ void Node::revert_virtual_loss(ChildIdx childIdx, float virtualLoss)
 float Node::scoreChildQValueMax(const Node* node, const SearchSettings* searchSettings) {
     float maxQValue = -2.0;
     for (int i = 0; i < d->qValues.size(); ++i) {
-        if (node->get_child_node(i) != nullptr) {
+        if (node->get_child_node(i) != nullptr && node->get_child_node(i) != NULL) {
             if (node->get_child_number_visits(i) >= searchSettings->maxAtVisit) {
                 maxQValue = max(maxQValue, node->d->qValues[i]);
             }
