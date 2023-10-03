c3a2a25b33c8efb172cab50ab5dce9d44c3ea94e
Felix Helfenstein <f.helfenstein@yahoo.de>
2023-10-03 20:27:53 +0200

    - added python file for executing cutechess shell commands

--- etc/generate_cutechess_command.py
+++ etc/generate_cutechess_command.py
@@ -16,8 +16,8 @@ player_b = "no_phases"
 
 pgnout = f"/data/cutechess_results/{player_a}_vs_{player_b}_movetime{movetime}_nodes{nodes}_bs{batch_size}.pgn"
 command = f"./cutechess-cli -variant standard -openings file=chess.epd format=epd order=random -pgnout {pgnout} -resign movecount=5 score=600 -draw movenumber=30 movecount=4 score=20 -concurrency 1 " \
-          f"-engine name=ClassicAra_{player_a} cmd=./ClassicAra dir=~/CrazyAra/engine/build option.Model_Directory=/data/model/ClassicAra/chess/{player_a} proto=uci " \
-          f"-engine name=ClassicAra_{player_b} cmd=./ClassicAra dir=~/CrazyAra/engine/build option.Model_Directory=/data/model/ClassicAra/chess/{player_b} proto=uci " \
+          f"-engine name=ClassicAra_{player_a} cmd=./FH_ClassicAra dir=~/CrazyAra/engine/build option.Model_Directory=/data/model/ClassicAra/chess/{player_a} proto=uci " \
+          f"-engine name=ClassicAra_{player_b} cmd=./FH_ClassicAra dir=~/CrazyAra/engine/build option.Model_Directory=/data/model/ClassicAra/chess/{player_b} proto=uci " \
           f"-each option.First_Device_ID={device} option.Batch_Size={batch_size} option.Fixed_Movetime={movetime} tc=0/6000+0.1 option.Nodes={nodes} option.Simulations={nodes*2} option.Search_Type=mcts -games 2 -rounds 500 -repeat " \
           f"|& tee {pgnout[:-4]}_output.txt"
 print(command)
