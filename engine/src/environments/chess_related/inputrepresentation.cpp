1a7ab6c43890eef8b4cfc9f3601b61396779b1c0
Felix Helfenstein <f.helfenstein@yahoo.de>
2023-12-19 16:18:35 +0100

    undone temporary fix for broken chess960 input representation

--- engine/src/environments/chess_related/inputrepresentation.cpp
+++ engine/src/environments/chess_related/inputrepresentation.cpp
@@ -538,12 +538,12 @@ inline void board_to_planes_chess_v3(PlaneData& planeData, size_t boardRepetitio
 #ifdef MODE_CHESS
     assert(planeData.current_channel() == StateConstants::NB_CHANNELS_POS() + StateConstants::NB_CHANNELS_CONST() + StateConstants::NB_LAST_MOVES() * StateConstants::NB_CHANNELS_PER_HISTORY());
 #endif
-    //set_960(planeData);
+    set_960(planeData);
 #ifdef MODE_CHESS
-    //assert(planeData.current_channel() == StateConstants::NB_CHANNELS_POS() + StateConstants::NB_CHANNELS_CONST() + StateConstants::NB_LAST_MOVES() * StateConstants::NB_CHANNELS_PER_HISTORY() + StateConstants::NB_CHANNELS_VARIANTS());
+    assert(planeData.current_channel() == StateConstants::NB_CHANNELS_POS() + StateConstants::NB_CHANNELS_CONST() + StateConstants::NB_LAST_MOVES() * StateConstants::NB_CHANNELS_PER_HISTORY() + StateConstants::NB_CHANNELS_VARIANTS());
 #endif
-    //set_piece_masks(planeData);
-    chess960_broken(planeData);
+    set_piece_masks(planeData);
+    //chess960_broken(planeData);
     set_checkerboard(planeData);
     set_material_diff(planeData);
     set_opposite_bishops(planeData);
