09c6810d6fa8f8d969a92134cc323bd4c139213a
Felix Helfenstein <f.helfenstein@yahoo.de>
2024-04-11 14:31:39 +0200

    - minor assertion change in train_cnn.py

--- DeepCrazyhouse/src/training/train_cnn.py
+++ DeepCrazyhouse/src/training/train_cnn.py
@@ -125,7 +125,8 @@ tc.log_metrics_to_tensorboard = True
 tc.export_grad_histograms = False
 
 phase_weights = {0: 1.0, 1: 1.0, 2: 1.0}  # specify the sample weight for each phase (will be normalized afterwards)
-assert len(phase_weights) == int(main_config["phase_definition"][-1])
+if "movecount" in main_config["phase_definition"]:
+    assert len(phase_weights) == int(main_config["phase_definition"][-1])
 
 # directory to write and read weights, logs, onnx and other export files
 #tc.export_dir = "C:/workspace/Python/CrazyAra/data/train_phase2/"
