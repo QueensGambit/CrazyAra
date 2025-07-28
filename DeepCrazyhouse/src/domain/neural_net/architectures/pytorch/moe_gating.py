e43e1106c3d3459a75c0c8e7fed646e66fca32f1
QueensGambit <curry-berry@freenet.de>
2025-07-28 22:36:02 +0200

    Make gating net even smaller + add MaxPooling

--- DeepCrazyhouse/src/domain/neural_net/architectures/pytorch/moe_gating.py
+++ DeepCrazyhouse/src/domain/neural_net/architectures/pytorch/moe_gating.py
@@ -8,7 +8,7 @@ A small simple gating network to evaluate having probabilities in our MoE / game
 """
 
 import torch
-from torch.nn import Sequential, Conv2d, BatchNorm2d, Linear
+from torch.nn import Sequential, Conv2d, BatchNorm2d, Linear, MaxPool2d
 
 from DeepCrazyhouse.src.domain.neural_net.architectures.pytorch.a0_resnet import ResidualBlock
 from DeepCrazyhouse.src.domain.neural_net.architectures.pytorch.builder_util import get_act, _Stem
@@ -19,7 +19,7 @@ class MoEGatingNet(torch.nn.Module):
     def __init__(
         self,
         n_labels=3,
-        channels=8,
+        channels=3,
         nb_input_channels=52,
         board_height=8,
         board_width=8,
@@ -29,17 +29,18 @@ class MoEGatingNet(torch.nn.Module):
     ):
         super(MoEGatingNet, self).__init__()
 
-        self.nb_flatten = phase_head_channels * board_width * board_height
+        self.nb_flatten = (phase_head_channels * board_width * board_height) // 4
 
         res_blocks = []
         for i in range(num_res_blocks):
             res_blocks.append(ResidualBlock(channels, act_type, use_se=False))
 
-        self.body = Sequential(_Stem(channels=channels, act_type=act_type,
-                                     nb_input_channels=nb_input_channels),
-                               *res_blocks)
-        self.final_body = Sequential(Conv2d(in_channels=channels, out_channels=phase_head_channels, kernel_size=(3, 3),
+        #self.body = Sequential(_Stem(channels=channels, act_type=act_type,
+        #                             nb_input_channels=nb_input_channels),
+        #                       *res_blocks)
+        self.final_body = Sequential(Conv2d(in_channels=nb_input_channels, out_channels=phase_head_channels, kernel_size=(3, 3),
                                             padding=(1, 1), bias=False),
+                                     MaxPool2d((2, 2), stride=2),
                                      BatchNorm2d(num_features=phase_head_channels),
                                      get_act(act_type))
         self.head = Sequential(Linear(in_features=self.nb_flatten, out_features=n_labels))
@@ -51,8 +52,8 @@ class MoEGatingNet(torch.nn.Module):
         :param x: Input to the ResidualBlock
         :return: Value & Policy Output
         """
-        out = self.body(x)
-        out = self.final_body(out).view(-1, self.nb_flatten)
+        #out = self.body(x)
+        out = self.final_body(x).view(-1, self.nb_flatten)
         return self.head(out)
 
 
