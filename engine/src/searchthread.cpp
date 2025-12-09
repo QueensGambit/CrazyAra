6d2f1b129af25b6c2d267362a0d8b96816621893
QueensGambit <curry-berry@freenet.de>
2025-12-09 14:54:27 +0100

    Avoid iteration

--- engine/src/searchthread.cpp
+++ engine/src/searchthread.cpp
@@ -430,7 +430,7 @@ void SearchThread::handle_fwd_pass()
     }
     cout << "*batchCounter: " << *batchCounter << endl;
     std::unique_lock<std::mutex> lock(*batchMutex);
-    ++*batchCounter;
+    *batchCounter = *batchCounter + 1;
     if (*batchCounter == searchSettings->numberParallelGames) {
         // query the network that corresponds to the majority phase
         nnUser->nets[select_nn_index()]->predict(nnUser->inputPlanes, nnUser->valueOutputs, nnUser->probOutputs, nnUser->auxiliaryOutputs);
