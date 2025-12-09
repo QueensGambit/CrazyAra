5501659f214eabc2beddba6b811dcd721dfa1036
QueensGambit <curry-berry@freenet.de>
2025-12-09 16:38:11 +0100

    Increase default batch size for RL

--- engine/src/uci/optionsuci.cpp
+++ engine/src/uci/optionsuci.cpp
@@ -67,7 +67,7 @@ void OptionsUCI::init(OptionsMap &o)
 {
     o["Allow_Early_Stopping"]          << Option(true);
 #ifdef USE_RL
-    o["Batch_Size"]                    << Option(8, 1, 8192);
+    o["Batch_Size"]                    << Option(64, 1, 8192);
 #else
 #ifdef OPENVINO
     o["Batch_Size"]                    << Option(16, 1, 8192);
