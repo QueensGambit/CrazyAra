3fea06e5f56f06847163ed718686a107417dff26
QueensGambit <curry-berry@freenet.de>
2026-01-09 13:37:57 +0100

    Update default value for MaxInitPly

--- engine/src/uci/optionsuci.cpp
+++ engine/src/uci/optionsuci.cpp
@@ -207,7 +207,7 @@ void OptionsUCI::init(OptionsMap &o)
     o["Centi_Resign_Probability"]      << Option(90, 0, 100);
     o["Centi_Resign_Threshold"]        << Option(-90, -100, 100);
     o["EPD_File_Path"]                 << Option("<empty>");
-    o["MaxInitPly"]                    << Option(30, 0, 99999);
+    o["MaxInitPly"]                    << Option(0, 0, 99999);  // 30
     o["MeanInitPly"]                   << Option(15, 0, 99999);
 #ifdef MODE_LICHESS
     o["Model_Directory_Contender"]     << Option((string("model_contender/" + engineName + "/") + get_first_variant_with_model()).c_str());
